e4b899ead5e1366c947d27a1bfd76fe411cab158
noufalrahim <noufalrahim6784@gmail.com>
2024-12-24 19:00:14 +0530

    chore: fixed unused imports

--- eslint.config.js
+++ eslint.config.js
@@ -3,30 +3,13 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-/** @type {import('eslint').Linter.FlatConfig[]} */
+
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-    languageOptions: {
-      globals: globals.browser,
-    },
-    rules: {
-      "no-debugger": "error", // Disallow debugger statements
-      "eqeqeq": ["error", "always"], // Require === and !==
-      "@typescript-eslint/no-unused-vars": [
-        "error",
-        {
-          vars: "all", // Check all variables
-          args: "after-used", // Ignore unused arguments if they are after used ones
-          varsIgnorePattern: "^React$", // Ignore React import warnings
-        },
-      ],
-      "react/react-in-jsx-scope": "off", // Disable React in scope rule for JSX (modern React versions)
-      "react/jsx-uses-react": "off", // Disable rule for unused React in JSX
-      "react/prop-types": "off", // Disable prop-types rule if you're using TypeScript
-    },
-  },
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  
 ];
