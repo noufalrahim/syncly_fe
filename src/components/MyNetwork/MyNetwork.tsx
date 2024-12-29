cfa8764f5e7475c51b00bd326018bdda3ad4faff
noufalrahim <noufalrahim6784@gmail.com>
2024-12-28 21:15:57 -0500

    fix: fiexed build issues

--- src/components/MyNetwork/MyNetwork.tsx
+++ src/components/MyNetwork/MyNetwork.tsx
@@ -15,6 +15,17 @@ import { Badge } from '../ui/badge';
 const MyNetwork = () => {
   const { isOpen, openModal, closeModal } = useModal();
   const [loading, setLoading] = useState(false);
+  const [suggestedPeople, setSuggestedPeople] = useState<Array<{
+    _id: string;
+    name: string;
+    username: string;
+    image: string;
+    projects: string[];
+    skills: Array<{
+      skillId: string;
+      skillName: string;
+    }>
+  }>>([]); // State to hold suggested people
   const [filteredPeople, setFilteredPeople] = useState<Array<{
     _id: string;
     name: string;
@@ -26,8 +37,6 @@ const MyNetwork = () => {
       skillName: string;
     }>
   }>>([]); // State to hold filtered people
-  const [searchTerm, setSearchTerm] = useState(''); // Search term for filtering
-
   // Filter options state
   const [selectedSkill, setSelectedSkill] = useState('');
   const [selectedProject, setSelectedProject] = useState('');
@@ -41,6 +50,7 @@ const MyNetwork = () => {
         const response = await getNetworks(authUser._id);
         console.log("Networks: ", response);
         setFilteredPeople(response.data.networks);
+        setSuggestedPeople(response.data.suggested);
       } catch (error) {
         console.log(error);
       }
@@ -52,13 +62,13 @@ const MyNetwork = () => {
   console.log("Initital fileterd: ", filteredPeople);
 
   // Handle search input change
-  const handleSearchChange = (e: any) => {
-    setSearchTerm(e.target.value);
-  };
+  // const handleSearchChange = (e: any) => {
+  //   setSearchTerm(e.target.value);
+  // };
 
   // Filter people based on selected skills and projects
   const handleFilter = () => {
-    let filtered = [...filteredPeople];
+    const filtered = [...filteredPeople];
 
     // if (selectedSkill) {
     //   filtered = filtered.filter((person) => person.skills.some((skill) => skill.toLowerCase().includes(selectedSkill.toLowerCase())));
@@ -100,30 +110,52 @@ const MyNetwork = () => {
         </div>
       ) : (
         <>
-          <h2 className="mb-4 text-2xl font-semibold text-gray-800">Network People</h2>
-
-          {/* Search Input */}
-          <div className="mb-4">
+        <h2 className="mb-4 text-2xl font-semibold text-gray-800">Suggested People</h2>
+          {/* <div className="mb-4">
             <input type="text" placeholder="Search by name, skill, or project..." className="w-full rounded-md border border-gray-300 p-2" value={searchTerm} onChange={handleSearchChange} />
+          </div> */}
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+            {
+              suggestedPeople.length > 0 && suggestedPeople.map((person, index) => (
+                <GeneralCard
+                  key={index}
+                  header={{
+                    image: person.image,
+                    title: person.name,
+                    description: person.username,
+                  }}
+                  buttons={[
+                    {
+                      title: 'Projects',
+                      onClick: () => {}
+                    },
+                    {
+                      title: 'Campaigns',
+                      onClick: () => {},
+                    },
+                    {
+                      title: 'Message',
+                      onClick: () => {},
+                    }
+                  ]}
+                >
+                  {
+                    person.skills && person.skills.length > 0 && (
+                      <div className="mt-2">
+                        {person.skills.map((skill) => (
+                          <Badge key={skill.skillId} className="mr-2">{skill.skillName}</Badge>
+                        ))}
+                      </div>
+                    )
+                  }
+                </GeneralCard>
+              ))
+            }
           </div>
 
-          {/* <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-            {filteredPeople.map((person, index) => (
-              <div key={index} className="rounded-md border border-gray-200 bg-white p-4 shadow-md">
-                <img src={person.image} alt={person.name} className="mb-4 h-16 w-16 rounded-full object-cover" />
-                <h3 className="text-lg font-semibold text-gray-800">{person.name}</h3>
-                {
-                  person.skills && person.skills.length > 0 && (
-                    <p className="mt-2 text-sm text-gray-600">
-                      <strong>Skills:</strong> {person.skills.map((skill) => skill.skillName).join(', ')}
-                    </p>
-                  )
-                }
-                <p className="mt-2 text-sm text-gray-600">
-                  <strong>Projects:</strong> {person.projects.join(', ')}
-                </p>
-              </div>
-            ))}
+          <h2 className="mb-4 text-2xl font-semibold text-gray-800">Network People</h2>
+          {/* <div className="mb-4">
+            <input type="text" placeholder="Search by name, skill, or project..." className="w-full rounded-md border border-gray-300 p-2" value={searchTerm} onChange={handleSearchChange} />
           </div> */}
           <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
             {
